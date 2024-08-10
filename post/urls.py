bfabdb3116c6651b12198d27562d86e9c693e871
Amir Hossein <amirhossein@Amirs-MBP.lan>
2024-08-10 22:40:39 +0330

    Make Delete

--- post/urls.py
+++ post/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import test, create_post
-
+from .views import home, create_post,delete
 
 urlpatterns = [
-    path('test/', test),
+    path('home/', home, name="home"),
+    path('delete/<int:post_id>', delete, name="delete"),
     path('', create_post)
 ]
--- post/views.py
+++ post/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import CreatePostForm
+from .models import Post
+from django.urls import reverse
+
+from .models import Post
 
 
-def test(request):
-    return render(request, 'test.html')
+def home(request):
+    return render(request, 'home.html', {"posts": Post.objects.all()})
+
+
+def delete(request, post_id):
+    obj = get_object_or_404(Post, id=post_id)
+    obj.delete()
+    print("Post was deleted!")
+    return redirect("home")
 
 
 def create_post(request):
@@ -11,9 +22,10 @@ def create_post(request):
     context['form'] = CreatePostForm()
     form = CreatePostForm(request.POST or None)
 
-    if form.is_valid():
-        form.save()
-        # print("Form Was Added")
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+        return redirect("home")
 
     context['form'] = form
 
